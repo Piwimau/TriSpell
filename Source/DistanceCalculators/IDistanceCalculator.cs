1992936b515c1681b90683ea28221c6783e1e244
Philipp Raschka <162504898+Piwimau@users.noreply.github.com>
2024-06-02 18:53:16 +0200

    Implement edit distance calculators.

---
+++ Source/DistanceCalculators/IDistanceCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TriSpell.Source.DistanceCalculators;
+
+/// <summary>Represents a calculator for determining the edit distance between two strings.</summary>
+/// <remarks>
+/// <para>
+/// The edit distance, also known as Levenshtein distance after Soviet mathematician Vladimir
+/// Levenshtein, is a measure of the similarity between two strings that may be used for basic
+/// spellchecking.
+/// </para>
+/// <para>
+/// More specifically, it is defined as the minimum number of single character edits (i. e.
+/// insertions, deletions and substitutions) required to transform a source string into a target.
+/// See <see href="https://en.wikipedia.org/wiki/Levenshtein_distance">Wikipedia</see> for more
+/// information.
+/// </para>
+/// </remarks>
+internal interface IDistanceCalculator {
+
+    /// <summary>Determines the edit distance between two given strings.</summary>
+    /// <remarks>
+    /// <para>
+    /// The edit distance is defined as the minimum number of single character edits (i. e.
+    /// insertions, deletions and substitutions) required to transform a source string into a target.
+    /// See <see href="https://en.wikipedia.org/wiki/Levenshtein_distance">Wikipedia</see> for more
+    /// information.
+    /// </para>
+    /// <para>
+    /// <example>
+    /// An example for using this method might be the following:
+    /// <code>
+    /// IDistanceCalculator distanceCalculator = ...; 
+    /// Console.WriteLine(distanceCalculator.Calculate("Cat", "Car")); // Output: 1
+    /// </code>
+    /// </example>
+    /// </para>
+    /// </remarks>
+    /// <param name="source">Source string to transform to <paramref name="target"/>.</param>
+    /// <param name="target">Target string to transform <paramref name="source"/> to.</param>
+    /// <returns>
+    /// The edit distance between <paramref name="source"/> and <paramref name="target"/>.
+    /// </returns>
+    int Distance(ReadOnlySpan<char> source, ReadOnlySpan<char> target);
+
+}
---
+++ Source/DistanceCalculators/RecursiveCalculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TriSpell.Source.DistanceCalculators;
+
+/// <summary>
+/// Represents a naive, recursive implementation of an <see cref="IDistanceCalculator"/>.
+/// </summary>
+/// <remarks>
+/// <para>
+/// This implementation is based on the original mathematical definition by Vladimir Levenshtein
+/// from 1965. Although the algorithm is relatively straightforward and easy to understand,
+/// it performs rather poorly, especially on larger input strings. This is due to the fact that
+/// a lot of edit distances between the prefixes of the strings are redundantly calculated.
+/// See <see href="https://en.wikipedia.org/wiki/Levenshtein_distance">Wikipedia</see> for more
+/// information.
+/// </para>
+/// <para>
+/// Note that this class is implemented as a singleton, as it does not feature any meaningful state
+/// that would justify having more than one instance at runtime.
+/// </para>
+/// </remarks>
+internal sealed class RecursiveCalculator : IDistanceCalculator {
+
+    /// <summary>Gets the instance of this <see cref="RecursiveCalculator"/> .</summary>
+    public static RecursiveCalculator Instance { get; } = new();
+
+    /// <summary>Initializes a new <see cref="RecursiveCalculator"/>.</summary>
+    /// <remarks>
+    /// Note that this constructor is marked <see langword="private"/>,
+    /// as the <see cref="RecursiveCalculator"/> is implemented as a singleton.
+    /// </remarks>
+    private RecursiveCalculator() { }
+
+    public int Distance(ReadOnlySpan<char> source, ReadOnlySpan<char> target) {
+        // If either of the strings is empty, the other can only be transformed into the same form
+        // by inserting or deleting all characters.
+        if (source.IsEmpty) {
+            return target.Length;
+        }
+        if (target.IsEmpty) {
+            return source.Length;
+        }
+        // First characters of source and target match, so the edit distance only depends on the
+        // remaining characters.
+        if (source[0] == target[0]) {
+            return Distance(source[1..], target[1..]);
+        }
+        // By now we know that at least the first character is different and we can calculate
+        // the edit distance by trying out all three possible actions.
+        int insertion = Distance(source, target[1..]);
+        int deletion = Distance(source[1..], target);
+        int substitution = Distance(source[1..], target[1..]);
+        return 1 + Math.Min(Math.Min(insertion, deletion), substitution);
+    }
+
+}
